330d97a30732001b3a8568e563fdac1569db901e
Gerald Landl <glandl@duality.at>
2022-06-09 20:14:00 +0200

    Create challenge-progress.py

---
+++ challenge-progress.py
@@ -0,0 +1,47 @@
+from cmath import e
+import sys
+from tkinter import CENTER, DoubleVar, Label, StringVar
+from turtle import distance
+if sys.version_info[0] == 3:
+    import tkinter as tk
+else:
+    import Tkinter as tk
+
+
+#basic setting for the Program
+fonsize = 500
+distance_total = 1378.50
+rounds_counted = 0
+distance_per_round = 0.75
+distance_elapsed = 0.0
+distance_remaining = distance_total
+
+#set up the window
+root=tk.Tk()
+root.title('UA Running Challenge 2022')
+
+dblDistanceElapsed = DoubleVar()
+dblDistanceElapsed.set(0.0)
+dblDistanceRemaining = DoubleVar()
+dblDistanceRemaining.set(distance_total)
+l_distance_elapsed = Label(root, justify=CENTER, compound=CENTER, textvariable=dblDistanceElapsed, bg="green", fg="white", font=("Arial", fonsize))
+l_distance_elapsed.pack(expand=1, fill="both")
+l_distance_remaining = Label(root, justify=CENTER, compound=CENTER, textvariable=dblDistanceRemaining, bg="red", fg="white", font=("Arial", fonsize))
+l_distance_remaining.pack(expand=1, fill="both")
+
+
+def calculate_distances(event):
+    global distance_elapsed, distance_remaining, rounds_counted
+    global dblDistanceElapsed, dblDistanceRemaining
+    rounds_counted += 1
+    distance_elapsed = rounds_counted * distance_per_round
+    distance_remaining = distance_total - distance_elapsed
+    dblDistanceElapsed.set(distance_elapsed)
+    dblDistanceRemaining.set(distance_remaining)
+    
+
+#binding the SPACE keypress event to the function
+root.bind('<space>', calculate_distances)
+
+#start the event loop
+root.mainloop()
