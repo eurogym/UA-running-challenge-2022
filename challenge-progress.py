42414544dd9c783b795669e1429921c89deb5f87
Gerald Landl <glandl@duality.at>
2022-06-09 21:06:50 +0200

    Update challenge-progress.py

--- challenge-progress.py
+++ challenge-progress.py
@@ -9,21 +9,21 @@ else:
 
 
 #basic setting for the Program
-fonsize = 500
+fonsize = 350
 distance_total = 1378.50
-rounds_counted = 0
 distance_per_round = 0.75
-distance_elapsed = 0.0
+distance_elapsed = 1500.0
 distance_remaining = distance_total - distance_elapsed
+rounds_counted = distance_elapsed / distance_per_round
 
 #set up the window
 root=tk.Tk()
 root.title('UA Running Challenge 2022')
 
 dblDistanceElapsed = DoubleVar()
-dblDistanceElapsed.set(0.0)
+dblDistanceElapsed.set(distance_elapsed)
 dblDistanceRemaining = DoubleVar()
-dblDistanceRemaining.set(distance_total)
+dblDistanceRemaining.set(distance_remaining)
 l_distance_elapsed = Label(root, justify=CENTER, compound=CENTER, textvariable=dblDistanceElapsed, bg="green", fg="white", font=("Arial", fonsize))
 l_distance_elapsed.pack(expand=1, fill="both")
 l_distance_remaining = Label(root, justify=CENTER, compound=CENTER, textvariable=dblDistanceRemaining, bg="red", fg="white", font=("Arial", fonsize))
